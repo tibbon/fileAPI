04334dfda6025aec4b2657b6437a54b0dedda548
David Fisher <tibbon@gmail.com>
2015-02-27 13:15:22 -0500

    Refactored fs_item controller slightly to dry up searching for individual item

--- app/controllers/fs_items_controller.rb
+++ app/controllers/fs_items_controller.rb
@@ -1,4 +1,5 @@
 class FsItemsController < ApplicationController
+  before_action :set_fs_item, only: [:destroy, :update, :show]
 
   def index
     @fs_items = FsItem.top_level_or_nested(params[:fs_item])
@@ -6,29 +7,30 @@ class FsItemsController < ApplicationController
   end
 
   def show
-    @fs_item = FsItem.find(params[:id])
     render json: @fs_item
   end
 
   def destroy
-    @fs_item = FsItem.find(params[:id]).delete
+    @fs_item.delete
     render json: {}, status: :no_content
   end
 
   def update
-    @fs_item = FsItem.find(params['id'])
     @fs_item.update_attributes(fs_item_params)
     render json: @fs_item
   end
 
   def create
     fs_item = FsItem.new(fs_item_params)
-
     # I should check the integrity here, but I'm currently not
     render json: fs_item
   end
 
   private
+  def set_fs_item
+    @fs_item = FsItem.find(params[:id])
+  end
+
   def fs_item_params
     params.require(:fs_item).permit(:name, :fs_item_id)
   end
